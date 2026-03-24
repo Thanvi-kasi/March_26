d5f663e70bbd0cc2d2bfd0b43dde80f5c029a4d9
Thanvi-kasi <kasithanvi01@gmail.com>
2026-03-24 22:24:25 +0530

    Create Palindrome Linked List.py

---
+++ Palindrome Linked List.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # Step 1: Find middle
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Step 2: Reverse second half
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+        
+        # Step 3: Compare both halves
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        
+        return True
